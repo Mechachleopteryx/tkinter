d40738edbd001b520d4a86e40f538abfbec8ee19
asad632 <74180557+asad632@users.noreply.github.com>
2020-12-01 21:43:26 +0400

    Update menu_bar.py

--- menu_bar.py
+++ menu_bar.py
@@ -1,19 +1,19 @@
-# import tkinter
-# from tkinter import ttk
-#
-# window = tkinter.Tk()
-# window.title('Main Menu')
-#
-# def func():
-#     print('function called ')
-# menubar = tkinter.Menu(window)
-# menubar.add_command(label='Save', command=func)
-# menubar.add_command(label='Copy', command=func)
-# menubar.add_command(label='Save', command=func)
-#
-#
-#
-#
-#
-# window.config(menu=menubar)
-# window.mainloop()
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+window.title('Main Menu')
+
+def func():
+    print('function called ')
+menubar = tkinter.Menu(window)
+menubar.add_command(label='Save', command=func)
+menubar.add_command(label='Copy', command=func)
+menubar.add_command(label='Save', command=func)
+
+
+
+
+
+window.config(menu=menubar)
+window.mainloop()
